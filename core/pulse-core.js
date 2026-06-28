b7ae17357e46bae7e20031579274fa393a520b47
djripster <djripster@outlook.com>
2026-06-28 10:35:47 -0400

    Update pulse-core.js

--- core/pulse-core.js
+++ core/pulse-core.js
@@ -1,1 +1,26 @@
+/*
+ * Pulse
+ * ------------------------------------
+ * Module: pulse-core.js
+ *
+ * Shared Pulse behaviour.
+ *
+ * Prototype: v0.1
+ *
+ * This module contains the public API used
+ * by Pulse implementations.
+ *
+ * UI belongs to the platform.
+ * Behaviour belongs to Core.
+ */
+Pulse.init()
 
+Pulse.isFirstLaunch()
+
+Pulse.shouldShowWelcome()
+
+Pulse.markVisit()
+
+Pulse.dismissWelcome()
+
+Pulse.getReaderState()
